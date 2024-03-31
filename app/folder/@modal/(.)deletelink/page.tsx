b55a3c4e58f9b14aa861a4f0677ab508f604681d
parkhyeonwoo <pq120wal39@gmail.com>
2024-03-31 22:47:50 +0900

    feat : 모달 url 경로 생성 및 모달 기능 구현

---
+++ app/folder/@modal/(.)deletelink/page.tsx
@@ -0,0 +1,11 @@
+import EditModal from "@/components/editmodal/EditModal";
+import ModalLayout from "@/components/modallayout/ModalLayout";
+import { MODAL_TYPE } from "@/constant";
+
+export default function ModalPage() {
+  return (
+    <ModalLayout>
+      <EditModal type={MODAL_TYPE[3]} />
+    </ModalLayout>
+  );
+}
